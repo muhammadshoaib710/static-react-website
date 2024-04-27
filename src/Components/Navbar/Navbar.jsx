b79cf47b8553600aa63fe93a51ac901194e1aeb7
SHOAIB UR REHMAN <01-135202-090@student.bahria.edu.pk>
2024-04-27 23:55:51 +0500

    Remove unnecessary link from Navbar in Navbar.jsx

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -16,7 +16,6 @@ return (
             <img src={logo} alt="" className='logo'/>
             <ul>
                     <li><Link to='hero' smooth={true} offset={0} duration={500} >Home</Link></li>
-                    <li><Link to='hero' smooth={true} offset={0} duration={500} >hahahahha</Link></li>
                     <li><Link to='about' smooth={true} offset={-150} duration={500} >About</Link></li>
                     <li><Link to='services' smooth={true} offset={0} duration={500} >Services</Link></li>
                     <li><Link to='campus' smooth={true} offset={-260} duration={500} >Campus</Link></li>   
